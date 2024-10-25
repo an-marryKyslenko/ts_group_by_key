15ca2e63b6762e38d759636aec72bd8be863c229
an-marryKyslenko <olih.a@icloud.com>
2024-10-25 14:08:11 +0200

    change logic

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -3,11 +3,13 @@ type GroupsMap<T> = {
 };
 
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
-  // write code here;
   return items.reduce((acc: GroupsMap<T>, item: T) => {
     const value = item[key] as string | number;
-    const sortedItem = items.filter((el) => el[key] === value);
 
-    return { ...acc, [value]: sortedItem };
+    if (!acc[value]) {
+      acc[value] = [];
+    }
+    acc[value].push(item);
+    return acc;
   }, {});
 }
