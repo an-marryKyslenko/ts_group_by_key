19f1a56201e00a8a5033a1504f54eeae677d81c1
an-marryKyslenko <olih.a@icloud.com>
2024-10-25 13:42:57 +0200

    add solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,13 @@
 type GroupsMap<T> = {
-  [key: string]: T[];
+  [key: string | number]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
+  return items.reduce((acc: GroupsMap<T>, item: T) => {
+    const value = item[key] as string | number;
+    const sortedItem = items.filter((el) => el[key] === value);
+
+    return { ...acc, [value]: sortedItem };
+  }, {});
 }
